136c0537bd674dbca74d1470afd780631dde827a
ロホマンシャヒン <shahin@MSI.localdomain>
2025-09-08 12:31:30 +0900

    config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'standalone',
+  output: "standalone",
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
