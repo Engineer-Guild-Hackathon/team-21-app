1adc728d0981c1299bcc4f1311e5dd24e16f67a1
Rohoman Shahin <shahin@example.com>
2025-09-10 09:26:21 +0900

    add:top

--- frontend/__tests__/Home.test.tsx
+++ frontend/__tests__/Home.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../src/app/page";
+import { render, screen } from '@testing-library/react';
+import Home from '../src/app/page';
 
-describe("Home", () => {
-  it("renders heading", () => {
+describe('Home', () => {
+  it('renders heading', () => {
     render(<Home />);
-    const heading = screen.getByRole("heading", {
+    const heading = screen.getByRole('heading', {
       name: /楽しみながら.*非認知能力.*を育む/i,
     });
     expect(heading).toBeInTheDocument();
